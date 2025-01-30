f8aa67e7fcef5fa9228ca3085400d3d556031c21
ermi-tekk21 <ermaatekk@gmail.com>
2025-01-30 09:45:55 +0300

    feat: enhance homepage layout and add new utility styles for Tailwind CSS

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,7 +2,10 @@
 export default function Home() {
   return (
     <>
-    Home
+      <section className="pink_container">
+        <h1 className="heading">Pitch Your Startup, <br />Connect With Enterpreneeurs</h1>
+        <p className="sub-heading !max-w-3xl">submit ideas, vote on pitches, and get noticed in virtual competitions</p>
+      </section>
     </>
   );
 }
