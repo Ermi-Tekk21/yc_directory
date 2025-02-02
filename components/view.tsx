cd5af41a9242a169fa962088942ae6c9fe595ff8
ermi-tekk21 <ermaatekk@gmail.com>
2025-02-02 23:12:43 +0300

    fix: adjust formatting in queries and view component for consistency

--- components/view.tsx
+++ components/view.tsx
@@ -14,7 +14,7 @@ const View = async ({ id }: { id: string }) => {
             </div>
 
             <p className="view-text">
-                <span className="!font-bold">Views: {totalViews}</span>
+                <span className="!font-bold ">Views: {totalViews}</span>
             </p>
         </div>
     )
