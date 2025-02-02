ece830c4f5928d5492763d5a63edbda3f03e2850
ermi-tekk21 <ermaatekk@gmail.com>
2025-02-02 11:18:02 +0300

    feat: update Next.js version, refactor queries, and enhance startup data fetching

--- sanity/lib/queries.ts
+++ sanity/lib/queries.ts
@@ -1,7 +1,7 @@
 import { defineQuery } from "next-sanity";
 
 export const STARTUPS_QUERY = defineQuery(
-    `*[
+  `*[
     _type == "startup" && defined(slug.current) && !defined($search)
     || title match $search 
     || category match $search 
@@ -12,7 +12,7 @@ export const STARTUPS_QUERY = defineQuery(
     title,
     slug,
     _createdAt,
-    author->{
+    author -> {
       _id,
       name,
       image,
@@ -20,6 +20,28 @@ export const STARTUPS_QUERY = defineQuery(
     },
     views,
     description,
+    category,
+    image
+  }`
+);
+
+export const STARTUP_BY_ID_QUERY = defineQuery(
+  `*[
+    _type == "startup" && _id == $id
+  ][0] {
+    _id,
+    title,
+    slug,
+    _createdAt,
+    author -> {
+      _id,
+      name,
+      image,
+      bio
+    },
+    views,
+    description,
+    pitch,
     category,
     image
   }`
--- sanity/types.ts
+++ sanity/types.ts
@@ -170,9 +170,9 @@ export type Markdown = string;
 
 export type AllSanitySchemaTypes = SanityImagePaletteSwatch | SanityImagePalette | SanityImageDimensions | SanityImageHotspot | SanityImageCrop | SanityFileAsset | SanityImageAsset | SanityImageMetadata | Geopoint | SanityAssetSourceData | Startup | Slug | Author | Markdown;
 export declare const internalGroqTypeReferenceTo: unique symbol;
-// Source: lib/queries.ts
+// Source: sanity/lib/queries.ts
 // Variable: STARTUPS_QUERY
-// Query: *[_type == "startup" && defined(slug.current) && !defined(slug.current) || title match $search || category match $search || autho->name match $search ] | order(_createdAt desc) {    _id,    title,    slug,    _createdAt,    author->{        _id,        name,        image,        bio    },    views,    description,    category,    image    }
+// Query: *[    _type == "startup" && defined(slug.current) && !defined($search)    || title match $search     || category match $search     || author->name match $search  ]   | order(_createdAt desc) {    _id,    title,    slug,    _createdAt,    author -> {      _id,      name,      image,      bio    },    views,    description,    category,    image  }
 export type STARTUPS_QUERYResult = Array<{
   _id: string;
   title: null;
@@ -209,11 +209,31 @@ export type STARTUPS_QUERYResult = Array<{
   category: string | null;
   image: string | null;
 }>;
+// Variable: STARTUP_BY_ID_QUERY
+// Query: *[    _type == "startup" && _id == $id  ][0] {    _id,    title,    slug,    _createdAt,    author -> {      _id,      name,      image,      bio    },    views,    description,    pitch,    category,    image  }
+export type STARTUP_BY_ID_QUERYResult = {
+  _id: string;
+  title: string | null;
+  slug: Slug | null;
+  _createdAt: string;
+  author: {
+    _id: string;
+    name: string | null;
+    image: string | null;
+    bio: string | null;
+  } | null;
+  views: number | null;
+  description: string | null;
+  pitch: string | null;
+  category: string | null;
+  image: string | null;
+} | null;
 
 // Query TypeMap
 import "@sanity/client";
 declare module "@sanity/client" {
   interface SanityQueries {
-    "*[_type == \"startup\" && defined(slug.current) && !defined(slug.current) || title match $search || category match $search || autho->name match $search ] | order(_createdAt desc) {\n    _id,\n    title,\n    slug,\n    _createdAt,\n    author->{\n        _id,\n        name,\n        image,\n        bio\n    },\n    views,\n    description,\n    category,\n    image\n    }": STARTUPS_QUERYResult;
+    "*[\n    _type == \"startup\" && defined(slug.current) && !defined($search)\n    || title match $search \n    || category match $search \n    || author->name match $search\n  ] \n  | order(_createdAt desc) {\n    _id,\n    title,\n    slug,\n    _createdAt,\n    author -> {\n      _id,\n      name,\n      image,\n      bio\n    },\n    views,\n    description,\n    category,\n    image\n  }": STARTUPS_QUERYResult;
+    "*[\n    _type == \"startup\" && _id == $id\n  ][0] {\n    _id,\n    title,\n    slug,\n    _createdAt,\n    author -> {\n      _id,\n      name,\n      image,\n      bio\n    },\n    views,\n    description,\n    pitch,\n    category,\n    image\n  }": STARTUP_BY_ID_QUERYResult;
   }
 }
